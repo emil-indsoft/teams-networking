30e35f404a9db86a9051b93a428dc7acef8e059e
Emil LASLAU <emil.laslau@indsoft.ro>
2023-06-26 12:25:12 +0300

    create load teams request

--- src/index.js
+++ src/index.js
@@ -1,2 +1,13 @@
 import "./style.css";
-console.warn("starting app");
+
+function loadTeams() {
+  const request = fetch("teams.json");
+  console.warn("request", request);
+  const response = request.then(r => r.json());
+  console.warn("response", response);
+  const r2 = response.then(result => {
+    console.warn("result", result);
+  });
+}
+
+loadTeams();
