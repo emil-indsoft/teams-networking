db9af7d9ec5852573c920b6f9e9e8797a8b09175
Emil LASLAU <emil.laslau@indsoft.ro>
2023-06-26 13:07:57 +0300

    displayTeams from json into table

--- src/index.js
+++ src/index.js
@@ -1,10 +1,30 @@
 import "./style.css";
 
+function $(selector) {
+  return document.querySelector(selector);
+}
+
+function getTeamAsHTML(team) {
+  return `<tr>
+    <td>${team.promotion}</td>
+    <td>${team.members}</td>
+    <td>${team.name}</td>
+    <td>${team.url}</td>
+    <td>&#10006 &#9998;</td>
+  </tr>`;
+}
+
+function displayTeams(teams) {
+  const teamsHTML = teams.map(getTeamAsHTML);
+  console.warn("teamsHTML", teamsHTML);
+  $("#teamsTable tbody").innerHTML = teamsHTML.join("");
+}
+
 function loadTeams() {
   fetch("teams.json")
     .then(r => r.json())
     .then(teams => {
-      console.warn("result", teams);
+      displayTeams(teams);
     });
 }
 
